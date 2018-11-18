23dc53b9b2facb44374ea737ed186bacf404dc30
Marek Baranowski <baranowski.marek98@gmail.com>
2018-11-18 18:51:55 +0100

    Added single task

---
+++ Todo/Models/Task.cs
@@ -0,0 +1,9 @@
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool isCompleate { get; set; }
+    }
+}
